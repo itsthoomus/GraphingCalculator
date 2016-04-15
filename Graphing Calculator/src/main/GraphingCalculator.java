3192da9bab9fefed93a4492d1bb3e590b1ff634a
McFeelio <owenjmcneil@gmail.com>
2016-04-15 09:22:40 -0400

    Initial GUI Added

--- Graphing Calculator/src/main/GraphingCalculator.java
+++ Graphing Calculator/src/main/GraphingCalculator.java
@@ -5,7 +5,7 @@ public class GraphingCalculator {
 
 	// Main Method, initially calls 
 	public static void main(String args[]) {
-		
+		MenuFrame frame = new MenuFrame();
 	}
 	
 	
---
+++ Graphing Calculator/src/main/MenuFrame.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MenuFrame extends JFrame {
+	
+	public MenuFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600,  300);
+		this.setLayout(null);
+		setVisible(true);
+		this.setResizable(false);
+		this.setTitle("Graphing Calculator");
+		
+		MenuPanel panel = new MenuPanel();
+		panel.setBounds(0, 0, 600, 300);
+		panel.setVisible(true);
+		add(panel);
+		repaint();
+	}
+	
+	class MenuPanel extends JPanel {
+		
+		public MenuPanel() {
+			JButton sciButton = new JButton("Scientific Mode");
+			sciButton.setBounds(200, 150, 200, 100);
+			add(sciButton);
+			
+		}
+		
+		
+		@Override
+		public void paintComponent(Graphics g) {
+			Font font = new Font("Impact", Font.BOLD, 65);
+			g.setFont(font);
+			g.drawString("Graphing Calculator", 35, 65);
+		}
+	}
+
+}
