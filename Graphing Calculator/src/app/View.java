68658503a339fdb5f53eccf7293fc23d0bd7aa02
alexbie <alexbie98@gmail.com>
2016-04-22 09:37:50 -0400

    Added images, button stuff.

--- Graphing Calculator/src/app/View.java
+++ Graphing Calculator/src/app/View.java
@@ -1,8 +1,14 @@
 package app;
 
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicArrowButton;
 
 /**
  * This class handles all the graphical elements of the program, containing all functions used to display things on the screen.
@@ -15,6 +21,20 @@ public class View {
 	public JPanel background;
 	public JFrame frame;
 	public ScreenPanel screenPanel;
+	
+	//panel to hold buttons
+	public JPanel movementButtons;
+	
+	//buttons for moving, tracing, and zooming\
+	public JButton leftButton;
+	public JButton upButton;
+	public JButton rightButton;
+	public JButton downButton;
+	
+	public JButton traceMoveToggleButton;
+	public JButton zooomInButton;
+	public JButton zoomOutButton;
+	
 	
 	public View(){
 		
@@ -52,6 +72,18 @@ public class View {
 		
 		screenPanel = new ScreenPanel(7,7,280);
 		frame.add(screenPanel);
+		
+		movementButtons = new JPanel();
+		movementButtons.setBounds(7, 290, 280, 50);;
+		movementButtons.setBackground(Color.BLACK);
+		frame.add(movementButtons);
+		
+		upButton = new JButton(new ImageIcon("res/up-arrow.png"));
+		upButton.setSize(5, 5);
+		upButton.setBounds(0, 0, 10, 10);
+		movementButtons.add(upButton);
+		
+		
 	
 		
 	}
